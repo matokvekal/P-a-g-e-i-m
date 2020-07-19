d8014cccf67726788635aeac9cdf08f3fffa8506
TOMER <mictavim@gmail.com>
2020-07-19 17:30:45 +0300

    test

--- src/components/main/tabale-elemnts/tables/Stars.js
+++ src/components/main/tabale-elemnts/tables/Stars.js
@@ -5,6 +5,7 @@ import './stars.css'
  const Stars = (props) => {
     const count=Number(props.stars);
     let stars = [];
+    debugger
     for (let i = 0; i < 5; ++i) {
         i<count?
         stars.push(<i className="fa fa-star checked" key={i}></i>):
